a1cf210072780cef78ad9a50d9f4d9bd607af8d2
Manav Chugh <manav.chugh@incedoinc.com>
2021-03-13 17:55:25 +0530

    Functional Component added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import ClassBasedCompDemo from './components/Types/ClassBasedCompDemo';
+import FunctionBasedCompDemo from './components/Types/FunctionBasedCompDemo';
 
 function App() {
   return (
@@ -9,6 +10,8 @@ function App() {
       <h1 className="display-4">Hello World</h1>
       <hr />
       <ClassBasedCompDemo />
+      <hr/>
+      <FunctionBasedCompDemo />
     </div>
   );
 }
---
+++ src/components/Types/FunctionBasedCompDemo.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const FunctionBasedCompDemo = () => {
+    return (
+        <div>
+            <h4> Function Based Component Works</h4>
+        </div>
+    );
+}
+
+export default FunctionBasedCompDemo;
