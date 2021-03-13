8fd21e5407e2d13fa7268e65e8e3bcda04af8597
Manav Chugh <manav.chugh@incedoinc.com>
2021-03-13 16:42:27 +0530

    class Component added

---
+++ src/components/Types/ClassBasedCompDemo.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class ClassBasedCompDemo extends Component {
+    render() {
+        return (
+            <div>
+                <h3>Class Based Component Works</h3>
+            </div>
+        );
+    }
+}
+
+export default ClassBasedCompDemo;
