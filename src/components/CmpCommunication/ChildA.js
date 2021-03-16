99b7189db1208832c642bc9efb81c4a4026e7cf7
Manav Chugh <manav.chugh@incedoinc.com>
2021-03-16 22:59:08 +0530

    Props Usage

--- src/components/CmpCommunication/ChildA.js
+++ src/components/CmpCommunication/ChildA.js
@@ -5,6 +5,9 @@ const ChildA = (props) => {
     return (
         <div>
             <h2>Child A Works</h2>
+            <p>Name : {props.name} </p>
+            <p>Age : {props.age} </p>
+            <button onClick={() => props.display()} >Click Me</button>
         </div>
     );
 }
--- src/components/CmpCommunication/Parent.js
+++ src/components/CmpCommunication/Parent.js
@@ -2,10 +2,15 @@ import React, { Component } from 'react';
 import ChildA from './ChildA';
 
 class Parent extends Component {
+    display = () =>{
+        alert("Who is this?");
+    }
+
     render() {
+        let age = 30;
         return (
             <div>
-                <ChildA name = "foo" />
+                <ChildA name = "foo" age = {age} display= {this.display} />
             </div>
         );
     }
